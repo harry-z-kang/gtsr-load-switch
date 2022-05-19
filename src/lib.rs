b5bb9a7df9357bdc5446d23ddc0f8e90abbbd055
Zixuan Kang <harry.z.kang@outlook.com>
2022-05-19 11:08:57 -0400

    Finish LoadSwitch struct definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use tm4c123x_hal as hal;
 
-use self::hal::gpio;
+use self::hal::prelude;
 
 pub const LS_OVERCURRENT_DISABLE: f64 = 99999.9;
 pub const LS_MIN_EXPECT_CURRENT_DISABLE: f64 = -99999.9;
@@ -14,6 +14,28 @@ enum LoadSwitchChannelStatus {
     LsStatusFaultOverCurrent = -3,
 }
 
-pub struct LoadSwitch<PXx> {
-    latch: PXx<gpio::Output<gpio::PushPull>>,
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+enum SensorEnablingStatus {
+    Chan1Sensing = 1, // sel[0] and sel[1] are not connected. Only channel 1 current sensing is available.
+    Chan12Sensing = 2, // sel[0] is not connected: Ch1,2 current sense enabled, temperature sensing not enabled
+    TempSensing = 3,   // sel[0] is connected, temperature sensing is enabled
+}
+
+#[allow(deprecated)]
+pub struct LoadSwitch<'a> {
+    latch: &'a dyn prelude::_embedded_hal_digital_OutputPin,
+    sel: [&'a dyn prelude::_embedded_hal_digital_OutputPin; 2],
+    en: [&'a dyn prelude::_embedded_hal_digital_OutputPin; 2],
+    dia_en: &'a dyn prelude::_embedded_hal_digital_OutputPin,
+    channel_state: SensorEnablingStatus,
+
+    overcurrent_threshold: [f64; 2],
+    min_expected_current: [f64; 2],
+    current: [f64; 2],
+    status: [LoadSwitchChannelStatus; 2],
+    latching: bool,
+    overcurrent_retries: [i8; 2],
+    temperature: f64,
+    current_scale_factor: f64,
+    temperature_scale_factor: f64,
 }
