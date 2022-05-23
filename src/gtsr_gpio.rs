68f98350985a22ee710a1c75a34174d68adf423e
Zixuan Kang <harry.z.kang@outlook.com>
2022-05-23 12:16:11 -0400

    Finally figure out the typing problem with the gpio pins

--- src/gtsr_gpio.rs
+++ src/gtsr_gpio.rs
@@ -2,29 +2,62 @@ use tm4c123x_hal as hal;
 
 use self::hal::gpio;
 
-pub trait GpioParts {}
+pub trait Pin {}
 
-impl GpioParts for gpio::gpioa::Parts {}
-impl GpioParts for gpio::gpiob::Parts {}
-impl GpioParts for gpio::gpioc::Parts {}
-impl GpioParts for gpio::gpiod::Parts {}
-impl GpioParts for gpio::gpioe::Parts {}
-impl GpioParts for gpio::gpiof::Parts {}
+impl Pin for gpio::gpioa::PA0<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioa::PA1<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioa::PA2<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioa::PA3<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioa::PA4<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioa::PA5<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioa::PA6<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioa::PA7<gpio::Output<gpio::PushPull>> {}
 
-pub struct Signal<T: GpioParts> {}
+impl Pin for gpio::gpiob::PB0<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiob::PB1<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiob::PB2<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiob::PB3<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiob::PB4<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiob::PB5<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiob::PB6<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiob::PB7<gpio::Output<gpio::PushPull>> {}
 
-impl<T: GpioParts> Signal<T> {
-    pub fn new(&self, parts: T, pin: u8) -> Self {
-        match pin {
-            0 => parts.pa0.into_push_pull_output(),
-            1 => parts.pa1.into_push_pull_output(),
-            2 => parts.pa2.into_push_pull_output(),
-            3 => parts.pa3.into_push_pull_output(),
-            4 => parts.pa4.into_push_pull_output(),
-            5 => parts.pa5.into_push_pull_output(),
-            6 => parts.pa6.into_push_pull_output(),
-            7 => parts.pa7.into_push_pull_output(),
-            _ => panic!("Invalid pin number"),
-        }
-    }
+// impl Pin for gpio::gpioc::PC0<gpio::Output<gpio::PushPull>> {}
+// impl Pin for gpio::gpioc::PC1<gpio::Output<gpio::PushPull>> {}
+// impl Pin for gpio::gpioc::PC2<gpio::Output<gpio::PushPull>> {}
+// impl Pin for gpio::gpioc::PC3<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioc::PC4<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioc::PC5<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioc::PC6<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioc::PC7<gpio::Output<gpio::PushPull>> {}
+
+impl Pin for gpio::gpiod::PD0<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiod::PD1<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiod::PD2<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiod::PD3<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiod::PD4<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiod::PD5<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiod::PD6<gpio::Output<gpio::PushPull>> {}
+// impl Pin for gpio::gpiod::PD7<gpio::Output<gpio::PushPull>> {}
+
+impl Pin for gpio::gpioe::PE0<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioe::PE1<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioe::PE2<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioe::PE3<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioe::PE4<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioe::PE5<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioe::PE6<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpioe::PE7<gpio::Output<gpio::PushPull>> {}
+
+// impl Pin for gpio::gpiof::PF0<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiof::PF1<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiof::PF2<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiof::PF3<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiof::PF4<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiof::PF5<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiof::PF6<gpio::Output<gpio::PushPull>> {}
+impl Pin for gpio::gpiof::PF7<gpio::Output<gpio::PushPull>> {}
+
+pub struct Signal<T: Pin> {
+    pin: T,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+#![feature(associated_type_bounds)]
 mod gtsr_gpio;
+
+use std::marker::PhantomData;
 
 pub const LS_OVERCURRENT_DISABLE: f64 = 99999.9;
 pub const LS_MIN_EXPECT_CURRENT_DISABLE: f64 = -99999.9;
@@ -26,11 +29,21 @@ pub enum SensorEnablingStatus {
 }
 
 #[allow(deprecated)]
-pub struct LoadSwitch<'a, T: gtsr_gpio::GpioParts> {
-    latch: &'a gtsr_gpio::Signal<T>,
-    sel: [&'a gtsr_gpio::Signal<T>; 2],
-    en: [&'a gtsr_gpio::Signal<T>; 2],
-    dia_en: &'a gtsr_gpio::Signal<T>,
+pub struct LoadSwitch<
+    'a,
+    L: gtsr_gpio::Pin,
+    S1: gtsr_gpio::Pin,
+    S2: gtsr_gpio::Pin,
+    EN1: gtsr_gpio::Pin,
+    EN2: gtsr_gpio::Pin,
+    DIA: gtsr_gpio::Pin,
+> {
+    latch: &'a gtsr_gpio::Signal<L>,
+    sel1: &'a gtsr_gpio::Signal<S1>,
+    sel2: &'a gtsr_gpio::Signal<S2>,
+    en1: &'a gtsr_gpio::Signal<EN1>,
+    en2: &'a gtsr_gpio::Signal<EN2>,
+    dia_en: &'a gtsr_gpio::Signal<DIA>,
     channel_state: SensorEnablingStatus,
 
     overcurrent_threshold: [f64; 2],
@@ -43,7 +56,21 @@ pub struct LoadSwitch<'a, T: gtsr_gpio::GpioParts> {
     current_scale_factor: f64,
     temperature_scale_factor: f64,
 
-    _data: std::marker::PhantomData<T>,
+    _latch_pin: PhantomData<L>,
+    _sel1_pin: PhantomData<S1>,
+    _sel2_pin: PhantomData<S2>,
+    _en1_pin: PhantomData<EN1>,
+    _en2_pin: PhantomData<EN2>,
+    _dia_en_pin: PhantomData<DIA>,
 }
 
-impl<T> LoadSwitch<'static, T: gtsr_gpio::GpioParts> {}
+impl<
+        L: gtsr_gpio::Pin,
+        S1: gtsr_gpio::Pin,
+        S2: gtsr_gpio::Pin,
+        EN1: gtsr_gpio::Pin,
+        EN2: gtsr_gpio::Pin,
+        DIA: gtsr_gpio::Pin,
+    > LoadSwitch<'static, L, S1, S2, EN1, EN2, DIA>
+{
+}
